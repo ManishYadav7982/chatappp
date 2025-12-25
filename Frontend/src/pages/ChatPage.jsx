20e766b71bf096e0d9b6a9506f5bce422dad187f
ManishYadav7982 <manishya12390@gmail.com>
2025-12-26 01:23:38 +0530

    add login and logout pages

--- Frontend/src/pages/ChatPage.jsx
+++ Frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 
 const ChatPage = () => {
+  const {logout} = useAuthStore();
+
   return (
     <div>
-      this is chat page 
+      <h1>Welcome to Chat Page</h1>
+      <button onClick={logout} className="px-4 py-2 bg-red-500 text-white rounded">
+        Logout
+      </button>
     </div>
   )
 }
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -8,6 +8,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
     checkAuth: async () => {
 
@@ -35,8 +36,36 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             console.error("Error signing up:", error);
             toast.error(error.response?.data?.message || "Signup failed.");
+            throw error;
         } finally {
             set({ isSigningUp: false });
         }
     },
+
+    Login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Login successful!");
+        } catch (error) {
+            console.error("Error logging in:", error);
+            toast.error(error.response?.data?.message || "Invalid credentials");
+            throw error; // 🔥 THIS LINE FIXES EVERYTHING
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post('/auth/logout');
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+        } catch (error) {
+            console.error("Error logging out:", error);
+            toast.error("Logout failed");
+        }
+    },
 }));
+
